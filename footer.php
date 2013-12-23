9be63a5c864481532f1fa5816ebe319be8bdee5d
Daigo Fujiwara <daigo@daigofujiwara.com>
2013-12-23 16:02:11 -0500

    Google analytics

--- footer.php
+++ footer.php
@@ -33,17 +33,14 @@ columns ">
 <?php wp_footer(); ?>
 
 <script>
-	(function($) {
-		$(document).foundation();
-	})(jQuery);
-	/* Google Analytics -- H5BP */
-	(function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
-            function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
-            e=o.createElement(i);r=o.getElementsByTagName(i)[0];
-            e.src='//www.google-analytics.com/analytics.js';
-            r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
-            ga('create','UA-8418273-3');ga('send','pageview');
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-8418273-3', 'daigo.org');
+  ga('send', 'pageview');
+
 </script>
-	
 </body>
 </html>
